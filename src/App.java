f7c854c2eceebcc080856fbb45b79b771f670584
Rafael Godoi Orbolato <rafael.orbolato@gmail.com>
2023-09-21 10:02:08 -0300

    Cria a classe AutomovelBasico

--- src/App.java
+++ src/App.java
@@ -1,24 +1,44 @@
 import poov.Automovel;
+import poov.AutomovelBasico;
 
 public class App {
     public static void main(String[] args) throws Exception {
         
-        Automovel a1 = new Automovel();
-        System.out.println(a1);
-        System.out.println(a1.getPreco());
+        // Automovel a1 = new Automovel();
+        // System.out.println(a1);
+        // System.out.println(a1.getPreco());
 
-        Automovel a2 = new Automovel("azul", "Fiat 147", "gas");
-        System.out.println(a2);
-        System.out.println(a2.getPreco());
+        // Automovel a2 = new Automovel("azul", "Fiat 147", "gas");
+        // System.out.println(a2);
+        // System.out.println(a2.getPreco());
 
-        if (a1.equals(a2)) {
+        // if (a1.equals(a2)) {
+        //     System.out.println("Iguais");
+        // } else {
+        //     System.out.println("DIferentes");
+        // }
+
+        // Automovel a3 = new Automovel("azul", "Fiat 147", "gas");
+        // if (a2.equals(a3)) {
+        //     System.out.println("Iguais");
+        // } else {
+        //     System.out.println("DIferentes");
+        // }
+
+        AutomovelBasico ab1 = new AutomovelBasico("vermelho", "Renault", "gasolina", true, true, true);
+        System.out.println(ab1);
+        System.out.println(ab1.getPreco());
+
+        AutomovelBasico ab2 = new AutomovelBasico("vermelho", "Renault", "gasolina", true, true, true);
+        AutomovelBasico ab3 = new AutomovelBasico("amarelo", "Renault", "alcool", true, false, true);
+
+        if (ab1.equals(ab2)) {
             System.out.println("Iguais");
         } else {
             System.out.println("DIferentes");
         }
 
-        Automovel a3 = new Automovel("azul", "Fiat 147", "gas");
-        if (a2.equals(a3)) {
+        if (ab2.equals(ab3)) {
             System.out.println("Iguais");
         } else {
             System.out.println("DIferentes");
---
+++ src/poov/AutomovelBasico.java
@@ -0,0 +1,90 @@
+package poov;
+
+public class AutomovelBasico extends Automovel {
+
+    private boolean retrovisorPassageiro;
+    private boolean limpadorTraseiro;
+    private boolean radioFM;
+
+    public AutomovelBasico(String cor, String modelo, String combustivel, boolean retrovisorPassageiro,
+            boolean limpadorTraseiro, boolean radioFM) {
+        super(cor, modelo, combustivel);
+        this.retrovisorPassageiro = retrovisorPassageiro;
+        this.limpadorTraseiro = limpadorTraseiro;
+        this.radioFM = radioFM;
+    }
+
+    public boolean isRetrovisorPassageiro() {
+        return retrovisorPassageiro;
+    }
+
+    public void setRetrovisorPassageiro(boolean retrovisorPassageiro) {
+        this.retrovisorPassageiro = retrovisorPassageiro;
+    }
+
+    public boolean isLimpadorTraseiro() {
+        return limpadorTraseiro;
+    }
+
+    public void setLimpadorTraseiro(boolean limpadorTraseiro) {
+        this.limpadorTraseiro = limpadorTraseiro;
+    }
+
+    public boolean isRadioFM() {
+        return radioFM;
+    }
+
+    public void setRadioFM(boolean radioFM) {
+        this.radioFM = radioFM;
+    }
+
+    @Override
+    public double getPreco() {
+        double preco = super.getPreco();
+        if (retrovisorPassageiro) {
+            preco += 100;
+        }
+        if (limpadorTraseiro) {
+            preco += 250;
+        }
+        if (radioFM) {
+            preco += 300;
+        }
+        return preco;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + "retrovisorPassageiro: " + retrovisorPassageiro + "\nlimpadorTraseiro: " + limpadorTraseiro
+                + "\nradioFM: " + radioFM;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = super.hashCode();
+        result = prime * result + (retrovisorPassageiro ? 1231 : 1237);
+        result = prime * result + (limpadorTraseiro ? 1231 : 1237);
+        result = prime * result + (radioFM ? 1231 : 1237);
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (!super.equals(obj))
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        AutomovelBasico other = (AutomovelBasico) obj;
+        if (retrovisorPassageiro != other.retrovisorPassageiro)
+            return false;
+        if (limpadorTraseiro != other.limpadorTraseiro)
+            return false;
+        if (radioFM != other.radioFM)
+            return false;
+        return true;
+    }
+
+}
